9539d0662a6cb9f596ec81c05ae5ad4462172706
xinhochen <xinhochen@gmail.com>
2022-03-01 09:38:31 +0800

    fix: fix ClassCastException

--- src/main/java/com/wuzhizhan/mybatis/definitionsearch/MapperDefinitionSearch.java
+++ src/main/java/com/wuzhizhan/mybatis/definitionsearch/MapperDefinitionSearch.java
@@ -2,7 +2,7 @@ package com.wuzhizhan.mybatis.definitionsearch;
 
 import com.intellij.openapi.application.QueryExecutorBase;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiTypeParameterListOwner;
+import com.intellij.psi.search.searches.DefinitionsScopedSearch;
 import com.intellij.psi.xml.XmlElement;
 import com.intellij.util.Processor;
 import com.intellij.util.xml.DomElement;
@@ -13,18 +13,19 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author yanglin
  */
-public class MapperDefinitionSearch extends QueryExecutorBase<XmlElement, PsiElement> {
+public class MapperDefinitionSearch extends QueryExecutorBase<XmlElement, DefinitionsScopedSearch.SearchParameters> {
 
     public MapperDefinitionSearch() {
         super(true);
     }
 
     @Override
-    public void processQuery(@NotNull PsiElement queryParameters, @NotNull Processor<? super XmlElement> consumer) {
-        if (!(queryParameters instanceof PsiTypeParameterListOwner)) return;
+    public void processQuery(@NotNull DefinitionsScopedSearch.SearchParameters queryParameters,
+                             @NotNull Processor<? super XmlElement> consumer) {
+        final PsiElement element = queryParameters.getElement();
 
         Processor<DomElement> processor = domElement -> consumer.process(domElement.getXmlElement());
 
-        JavaService.getInstance(queryParameters.getProject()).process(queryParameters, processor);
+        JavaService.getInstance(queryParameters.getProject()).process(element, processor);
     }
 }
